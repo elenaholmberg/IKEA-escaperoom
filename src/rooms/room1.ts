d5c5dc0649d7bedbdf4fd1dd8e3772e1f694d9ec
elena holmberg <elenaholmberg83@hotmail.com>
2026-02-18 16:30:24 +0100

    ✅ starta knapp works

--- src/rooms/room1.ts
+++ src/rooms/room1.ts
@@ -10,4 +10,9 @@ export default function initRoom1() {
   const introductionDiv = document.getElementById("introductionDiv");
   const startRoom1Btn = document.getElementById("startRoom1Btn");
   const map = document.getElementById("map");
+
+  startRoom1Btn?.addEventListener("click", () => {
+    introductionDiv?.remove();
+    map?.classList.remove("hidden");
+  });
 }
